55261bb8c98e4f1ea7e158574b726ff293df87ff
yusuf <yusuf.salie96@gmail.com>
2021-07-07 10:16:08 +0200

    update

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Customer, Product, order
+from .models import Customer, Product, order, Tag
 
 admin.site.register(Customer)
 admin.site.register(Product)
+admin.site.register(Tag)
 admin.site.register(order)
