aefe888627aef471da7301081b71609887ab4782
yusuf <yusuf.salie96@gmail.com>
2021-07-14 09:01:01 +0200

    update

--- accounts/filters.py
+++ accounts/filters.py
@@ -1,10 +1,12 @@
 import django_filters
 from .models import*
-from django_filters import DateFilter
+from django_filters import DateFilter, CharFilter
 
 class OrderFilter(django_filters.FilterSet):
+    start_date = DateFilter(field_name="date_created",lookup_expr='gte')
+    end_date = DateFilter(field_name="date_created",lookup_expr='lte')
+    note = CharFilter(field_name='note', lookup_expr='icontains')
     class Meta:
-        start_date = DateFilter
         model = order
         fields = '__all__'
         exclude = ['customer','date_created']
